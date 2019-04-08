bf225dfa6df5c74cea29924533eeddbeec7ccce8
Andy Richardson <andy.john.richardson@gmail.com>
2019-04-08 21:06:28 +0100

    Add more extensive testing & add cache-first / network-first policies

--- src/useFetch.test.tsx
+++ src/useFetch.test.tsx
@@ -1,35 +1,38 @@
-jest.mock('react', () => {
-  const addResponse = jest.fn();
-  const clearDomains = jest.fn();
-
-  return {
-    ...jest.requireActual('react'),
-    useContext: () => ({
-      config: {
-        baseUrl: 'http://example',
-        headers: { accept: 'application/json' },
-      },
-      responses: {},
-      addResponse,
-      clearDomains,
-    }),
-  };
-});
-import React, { useContext } from 'react';
+jest.mock('./util', () => ({
+  getKey: jest.fn(),
+  executeRequest: jest.fn(),
+}));
+import React, { FC } from 'react';
 import renderer from 'react-test-renderer';
 import { useFetch, FetchState } from './useFetch';
+import { TippleContext } from './context';
+import { getKey, executeRequest } from './util';
 
-beforeEach(jest.clearAllMocks);
+// Setup util mocks
+const key = '12345';
+const response = 'value';
+(getKey as jest.Mock).mockReturnValue(key);
+(executeRequest as jest.Mock).mockReturnValue(Promise.resolve(response));
 
+// Provider mocks
+let config: any = {
+  baseUrl: 'http://url',
+  headers: { 'content-type': 'application/json' },
+};
+let responses: any = { '12345': [1, 2, 3] };
+let domains: any = {};
+let addResponse = jest.fn();
+let clearDomains = jest.fn();
+
+// Hook options
+let url: string = '/users';
+let opts: Parameters<typeof useFetch>[1] = { domains: ['any'] };
+
+// Hook results
 let state: FetchState;
 let refetch: () => void;
 
-type params = Parameters<typeof useFetch>;
-
-const Fixture: React.FC<{ url: params[0]; opts: params[1] }> = ({
-  url,
-  opts,
-}) => {
+const HookFixture: FC = () => {
   const [a, b] = useFetch(url, opts);
 
   state = a;
@@ -38,41 +41,123 @@ const Fixture: React.FC<{ url: params[0]; opts: params[1] }> = ({
   return null;
 };
 
-declare const global: any;
-global.fetch = jest.fn().mockResolvedValue([]);
+const Fixture: FC = props => (
+  <TippleContext.Provider
+    value={{ config, responses, domains, addResponse, clearDomains }}
+  >
+    <HookFixture />
+  </TippleContext.Provider>
+);
 
-describe('get request', () => {
-  describe('on init', () => {
-    const url = '/users';
-    const opts = { domains: ['1234'], fetchOptions: { method: 'GET' } };
-    const domainArgs = {};
-    let instance: renderer.ReactTestRenderer;
+const waitForAsync = (delay = 20) =>
+  new Promise(resolve => setTimeout(resolve, delay));
 
-    beforeEach(() => {
-      instance = renderer.create(<Fixture url={url} opts={opts} />);
+beforeEach(jest.clearAllMocks);
+
+describe('on init', () => {
+  let instance: renderer.ReactTestRenderer;
+
+  beforeEach(() => {
+    instance = renderer.create(<Fixture />);
+  });
+
+  afterEach(() => instance.unmount());
+
+  it('defaults to fetching', () => {
+    expect(state.fetching).toBe(true);
+  });
+
+  it('calls executeRequest', async () => {
+    instance.update(<Fixture />);
+    expect(executeRequest).toBeCalledTimes(1);
+  });
+
+  it('calls executeRequest with url and opts', async () => {
+    instance.update(<Fixture />);
+    expect(executeRequest).toBeCalledWith(`${config.baseUrl}${url}`, {
+      ...opts.fetchOptions,
+      headers: config.headers,
+    });
+  });
+
+  describe('addResponse', () => {
+    it('is called', async () => {
+      instance.update(<Fixture />);
+      await waitForAsync();
+      expect(addResponse).toBeCalledTimes(1);
     });
 
-    afterEach(() => {
-      instance.unmount();
-    });
-
-    it('defaults to fetching', () => {
-      expect(state.fetching).toBe(true);
-    });
-
-    it('calls fetch', async () => {
-      instance.update(<Fixture url={url} opts={opts} />);
-      expect(fetch).toBeCalledTimes(1);
-    });
-
-    it('calls fetch with url', async () => {
-      instance.update(<Fixture url={url} opts={opts} />);
-      expect(fetch).toBeCalledWith(`http://example${url}`, {
-        ...opts.fetchOptions,
-        headers: {
-          accept: 'application/json',
-        },
+    it('is passed args', async () => {
+      instance.update(<Fixture />);
+      await waitForAsync();
+      expect(addResponse).toBeCalledWith({
+        data: response,
+        key: key,
+        domains: opts.domains,
       });
     });
+  });
+});
+
+describe('on fetched', () => {
+  let instance: renderer.ReactTestRenderer;
+
+  beforeEach(() => {
+    instance = renderer.create(<Fixture />);
+  });
+
+  afterEach(() => instance.unmount());
+
+  it('sets fetching to false', async () => {
+    instance.update(<Fixture />);
+    await waitForAsync();
+    expect(state.fetching).toBe(false);
+  });
+
+  it('returns cached data', () => {
+    expect(state.data).toEqual(responses[key]);
+  });
+});
+
+describe('on error', () => {
+  let instance: renderer.ReactTestRenderer;
+
+  beforeEach(() => {
+    (executeRequest as jest.Mock).mockReturnValueOnce(Promise.reject(Error()));
+    instance = renderer.create(<Fixture />);
+  });
+
+  afterEach(() => instance.unmount());
+
+  it('sets fetching to false', async () => {
+    instance.update(<Fixture />);
+    await waitForAsync();
+    expect(state.fetching).toBe(false);
+  });
+
+  it('returns error value', async () => {
+    instance.update(<Fixture />);
+    await waitForAsync();
+    // @ts-ignore
+    expect(state.error.constructor).toBe(Error);
+  });
+});
+
+describe('on invalidation', () => {
+  let instance: renderer.ReactTestRenderer;
+
+  beforeEach(() => {
+    instance = renderer.create(<Fixture />);
+  });
+
+  afterEach(() => instance.unmount());
+
+  it('calls executeRequest again', async () => {
+    instance.update(<Fixture />);
+    await waitForAsync();
+    responses[key] = undefined;
+    instance.update(<Fixture />);
+    await waitForAsync();
+    expect(executeRequest).toBeCalledTimes(2);
   });
 });
--- src/useFetch.ts
+++ src/useFetch.ts
@@ -13,8 +13,9 @@ export interface FetchState<T = any> {
   error?: Error;
 }
 
-interface UseFetchOptions<D extends string> {
+interface UseFetchOptions<D extends string = string> {
   domains: D[];
+  cachePolicy?: 'cache-first' | 'network-first';
   onMount?: boolean;
   fetchOptions?: RequestInit;
 }
@@ -37,7 +38,24 @@ export const useFetch = <T = any, D extends string = string>(
   ]);
 
   /** Data parsed from cache/request. */
-  const data = useMemo(() => responses[key], [responses, key]);
+  useMemo(() => {
+    if (
+      responses[key] === undefined ||
+      JSON.stringify(state.data) === JSON.stringify(responses[key])
+    ) {
+      return;
+    }
+
+    // Cache first and data changed
+    if (opts.cachePolicy === 'cache-first' || opts.cachePolicy === undefined) {
+      return setState({ ...state, data: responses[key] });
+    }
+
+    // Network first and data changed
+    if (opts.cachePolicy === 'network-first' && state.data !== undefined) {
+      return setState({ ...state, data: responses[key] });
+    }
+  }, [state, opts.cachePolicy, responses[key]]);
 
   /** Executes fetching of data. */
   const doFetch = useCallback(async () => {
@@ -48,11 +66,13 @@ export const useFetch = <T = any, D extends string = string>(
         ...opts.fetchOptions,
         headers: { ...config.headers, ...(opts.fetchOptions || {}).headers },
       });
-
       addResponse({ data: response, key, domains: opts.domains });
-      setState({ fetching: false });
+      setState({
+        fetching: false,
+        data: opts.cachePolicy === 'network-first' ? response : undefined,
+      });
     } catch (error) {
-      setState({ ...state, error });
+      setState({ ...state, fetching: false, error });
     }
   }, [state, opts.domains, addResponse]);
 
@@ -63,10 +83,21 @@ export const useFetch = <T = any, D extends string = string>(
 
   /** On data change. */
   useEffect(() => {
-    if (!state.fetching && data === undefined) {
+    if (!state.fetching && state.data === undefined) {
       doFetch();
     }
-  }, [JSON.stringify(data), state.fetching]);
+  }, [state.data, state.fetching]);
 
-  return [{ ...state, data }, doFetch];
+  /** On cache invalidation. */
+  useEffect(() => {
+    if (
+      !state.fetching &&
+      state.data !== undefined &&
+      responses[key] === undefined
+    ) {
+      doFetch();
+    }
+  }, [state.data, state.fetching, responses[key]]);
+
+  return [{ ...state }, doFetch];
 };
